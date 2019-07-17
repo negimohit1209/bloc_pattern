fc7ecb18384ee4a92408c8be86ecd701a9994f5c
negimohit1209 <mohitsngh1997@gmail.com>
2019-07-17 13:42:16 +0530

    initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:login_stateful/src/screens/login_scree.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Login form",
+      home: Scaffold(
+        body: LoginScreen(),
+      )
+    );
+  }
+}
