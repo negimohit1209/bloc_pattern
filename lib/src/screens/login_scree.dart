dd69941781acff0ca39e91c0ad43d3f3096a4155
negimohit1209 <mohitsngh1997@gmail.com>
2019-07-17 17:25:27 +0530

    add: validator to the email and password field

--- lib/src/screens/login_scree.dart
+++ lib/src/screens/login_scree.dart
@@ -6,13 +6,73 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20),
       child: Form(
-        
+        key: formKey,
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            SizedBox(height: 10,),
+            submitButton(),
+          ],
+        ),
       )
     );
   }
+
+  Widget emailField(){
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@email.com'
+      ),
+      validator: (String value) {
+        if(! value.contains('@')) {
+          return 'please enter a valid email address';
+        }
+      },
+      onSaved: (String value) {
+        email = value;
+      },
+    );
+  }
+  Widget passwordField(){
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+          labelText: 'Password',
+          hintText: 'Password'
+      ),
+      validator: (String value){
+        if(value.length < 4){
+          return 'Password length should be greater then 4';
+        }
+      },
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
+  Widget submitButton(){
+    return RaisedButton(
+      color: Colors.blue,
+      onPressed: (){
+        if (formKey.currentState.validate()){
+          formKey.currentState.save();
+          print("consome them $email and $password");
+        }
+      },
+      child: Text("Submit", style: TextStyle(color: Colors.white),),
+    );
+  }
+
 }
